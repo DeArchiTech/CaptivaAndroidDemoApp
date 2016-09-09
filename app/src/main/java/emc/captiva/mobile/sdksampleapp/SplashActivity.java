d30940c8b37fc1c67f3b91f0d6925c57fbd7e190
david <noobiehacker>
2016-09-09 10:36:34 -0700

    Updated App Name, Icon , and add launch screen

---
+++ app/src/main/java/emc/captiva/mobile/sdksampleapp/SplashActivity.java
@@ -0,0 +1,40 @@
+package emc.captiva.mobile.sdksampleapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by david on 9/9/16.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
